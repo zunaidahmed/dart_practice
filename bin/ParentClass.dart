91d6ceaa38f1a63722eff95cec1a55bf7954d49f
dart <zunaidahmmed@gmail.com>
2023-11-05 00:22:34 +0600

    Update

---
+++ bin/ParentClass.dart
@@ -0,0 +1,29 @@
+
+
+class ParentClass {
+
+void name(){print("abu");}
+void age(){print(10);}
+void number(){
+
+  int a = 1;
+
+  print(a);
+
+
+}
+
+
+    ParentClass(){
+
+  int num = 2;
+
+  print(num);
+
+  print('Zunaid');
+
+
+}
+
+}
+
