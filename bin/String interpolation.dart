5a5c3934f6d4040ee71039af5c7b2a59cb468b39
dart <zunaidahmmed@gmail.com>
2023-11-08 18:31:33 +0600

    String interpolation

---
+++ bin/String interpolation.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main() {
+  String userName = 'Rahim';
+
+
+  String address = 'Mirpur, Dhaka';
+  int age = 25;
+
+  String fullDetails = userName + ' - ' + address; // contract
+  print(fullDetails + ' - ' + '$age');
+  print(fullDetails + ' - ' + age.toString());
+
+  String userDetailsSecondary = 'User name is' + userName + ' and ' +
+      age.toString() + ' is age';
+  userDetailsSecondary =
+  'User name is $userName and Age is $age'; // String interpolation
+
+  print(userDetailsSecondary);
+  print(userDetailsSecondary.length);
+}
