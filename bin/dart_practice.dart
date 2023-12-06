54650b3b2af23df7cb7f106fb63180b9a4513e1d
dart <zunaidahmmed@gmail.com>
2023-12-06 19:52:13 +0600

    getter setter

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,57 +1,33 @@
 
-
-
-
-
 import 'dart:io';
 
 void main() {
 
-  stdout.write('Type a Year = ');
+  Student One = Student();
 
-  int? year = int.parse(stdin.readLineSync()!);
+  One.setAge = (30+20/3);
 
-  if (year % 4 == 0){
+  print(One.getAdd);
 
-    print('$year is  Leap Year');
-  } else {
 
-    print('$year is not Leap Year');
+}
+
+
+class Student {
+
+  double? add;
+
+  double get getAdd{
+
+    return add!;
   }
 
-  }
+ void set setAge(double age){
+
+    this.add = age ;
 
 
 
+ }
 
-
-
-
-/*print('Please Enter your number');
-
-int? number = int.parse(stdin.readLineSync()!);
-
-for(int multi = 1 ; multi <= 10; multi++){
-
-print('$number * $multi = ${number*multi}');
-
-}*/
-
-
-
-
-/*print('Please Enter Your Number');
-
-  int? number = int.parse(stdin.readLineSync()!);
-
-  if( number % 2 == 0 ){
-
-    print('The Number is জোড়');
-
-  } else {
-
-    print('The Number is বিজোড়');
-  }*/
-
-
-
+}
--- test/dart_practice_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_practice/dart_practice.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
