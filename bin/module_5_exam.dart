cd6f489f2d4cb4025ada238fe90f425fabaa67c3
dart <zunaidahmmed@gmail.com>
2023-12-12 00:14:51 +0600

    module_5_exam.dart

--- bin/module_5_exam.dart
+++ bin/module_5_exam.dart
@@ -1,5 +1,51 @@
-void main (){
 
-  print ('Zunaid');
+
+void main() {
+
+
+  print('Original Fruit Details before Discount:');
+  displayFruitDetails([
+    { 'Name: Apple', 'Color: Red', 'Price:  \$2.5'},
+    { 'Name: Banana', 'Color: Yellow', 'Price: \$1.0'},
+    { 'Name: Grapes', 'Color: Purple', 'Price: \$3.0'}
+
+  ]);
+
+  print('Fruit Details After Applying 10% Discount:');
+  applyPriceDiscount([
+    { 'Name: Apple', 'Color: Red', 'Price:' '${2.5}'},
+    { 'Name: Banana', 'Color: Yellow', 'Price:' '${0.9}'},
+    { 'Name: Grapes', 'Color: Purple', 'Price:' '${2.7}'},
+  ]);
+
+
 
 }
+
+ void displayFruitDetails(List fruits){
+
+  print(fruits);
+
+ }
+
+void applyPriceDiscount(List discount){
+
+  for (int i = 0; i<=discount.length;i++){
+
+  }
+
+  print(discount);
+
+}
+
+
+
+
+  /*for (int i = 0; i <= mapInsideList.length; i++) {}
+}
+
+void person(List x) {
+  print(x);
+}*/
+
+
