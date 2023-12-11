23343ea8cc3a9ca5b4b2af5e90139187c8bc3647
dart <zunaidahmmed@gmail.com>
2023-12-12 00:39:24 +0600

    module_5_exam.dart

--- bin/module_5_exam.dart
+++ bin/module_5_exam.dart
@@ -12,9 +12,9 @@ void main() {
 
   print('Fruit Details After Applying 10% Discount:');
   applyPriceDiscount([
-    { 'Name: Apple', 'Color: Red', 'Price:' '${(2.5-10/100)}'},
-    { 'Name: Banana', 'Color: Yellow', 'Price:' '${0.9}'},
-    { 'Name: Grapes', 'Color: Purple', 'Price:' '${2.7}'},
+    { 'Name: Apple', 'Color: Red', 'Price:' '${2.5-10/100}'},
+    { 'Name: Banana', 'Color: Yellow', 'Price:' '${1.0-10/100}'},
+    { 'Name: Grapes', 'Color: Purple', 'Price:' '${3.0-10/100}'},
   ]);
 
 
