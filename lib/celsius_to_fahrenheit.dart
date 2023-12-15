1446ad05ea02f955d23b81686da21fe998e87fe0
dart <zunaidahmmed@gmail.com>
2023-12-15 11:03:55 +0600

    codemama_problem_solve

---
+++ lib/celsius_to_fahrenheit.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+
+
+  String num = stdin.readLineSync()!;
+  double celcius = double.parse(num);
+
+ double toFahrenheit = (celcius* 9/5 + 32);
+
+  print('The temperature in Fahrenheit is:${toFahrenheit.toStringAsFixed(2)}');
+
+}
---
+++ lib/minimal_yet_important.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  // Write your dart code from here
+  String userInput = stdin.readLineSync()!;
+  List<String> list = userInput.split(' ');
+
+  int numOne = int.parse(list[0]);
+  int numTwo = int.parse(list[1]);
+  int numThree = int.parse(list[2]);
+
+
+  if (numOne < numTwo && numOne < numThree ){
+    print (numOne);
+  }else if (numTwo < numOne && numTwo < numThree ){
+    print(numTwo);
+  } else if (numThree < numOne && numThree < numTwo){
+    print (numThree);
+  }
+
+
+}
